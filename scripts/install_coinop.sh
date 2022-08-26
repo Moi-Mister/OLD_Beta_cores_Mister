6c991d4550c9d05c078403f870a1dea95775016b
Moi-Mister <111741841+Moi-Mister@users.noreply.github.com>
2022-08-26 21:26:56 +0200

    Add files
    Add Coinop script

---
+++ scripts/install_coinop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+clear
+launchdir=$(pwd)
+#echo "launchdir : $launchdir"
+
+ZP="coinop.zip"
+
+cd /media/fat/scripts
+rm -r coinop 2> /dev/null
+rm $ZP  2> /dev/null
+
+curl -s https://raw.githubusercontent.com/funkycochise/Coin-Op/master/$ZP -O -k
+unzip -qq $ZP -d /media/fat/scripts/ 
+
+rm $ZP
+
+#echo "launchdir : $launchdir"
+cd /media/fat/scripts/coinop
+sh ./_run.sh $launchdir
+
+
+if test -f "$launchdir/update_main_mister.sh"; then
+   sh $launchdir/update_main_mister.sh >/dev/null
+fi
+
